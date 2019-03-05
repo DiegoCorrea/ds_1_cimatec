8b1a718640afb430cf4b36140bc449449ce07ee9
Diego Corrêa <diegocorrea.cc@gmail.com>
2019-03-05 17:27:16 -0300

    exercicio da aula numero 5

--- aula_5/Programa_Conta_Private/src/programa_conta_private/Conta.java
+++ aula_5/Programa_Conta_Private/src/programa_conta_private/Conta.java
@@ -1,49 +1,67 @@
 package programa_conta_private;
 
 public class Conta {
-    int numero;
-    String titular;
-    double saldo;
-    double limite;
-    double limiteFixo;
-    int tipoConta; // [0 - 3] Estudante, Trabalhador, Especial, Ilimitado
+    private final int NUMERO;
+    private final String TITULAR;
+    private double saldo;
+    private double limite;
+    private double limiteFixo;
+    private int tipoConta; // [0 - 3] Estudante, Trabalhador, Especial, Ilimitado
     
-    Conta(){
-        System.out.println("Construtor da classe");
+    Conta(int numero, String titular, double saldo) {
+        this.NUMERO = numero;
+        this.TITULAR = titular;
+        this.saldo = saldo;
+        this.limite = 0.0;
+        this.limiteFixo = 0.0;
+        this.tipoConta = 1;
     }
-    
+
     Conta(int numero, String titular, double saldo, double limiteFixo) {
-        this.numero = numero;
-        this.titular = titular;
+        this.NUMERO = numero;
+        this.TITULAR = titular;
         this.saldo = saldo;
         this.limite = limiteFixo;
         this.limiteFixo = limiteFixo;
-        this.tipoConta = 0;
+        this.tipoConta = 1; // Trabalhador
     }
     
     Conta(int numero, String titular, double saldo,double limiteFixo, int tipoConta) {
-        this.numero = numero;
-        this.titular = titular;
+        this.NUMERO = numero;
+        this.TITULAR = titular;
         this.saldo = saldo;
         this.limite = limiteFixo;
         this.limiteFixo = limiteFixo;
         this.tipoConta = tipoConta;
     }
+    // gets
     
-    Conta(int numero, String titular, double saldo) {
-        this.numero = numero;
-        this.titular = titular;
-        this.saldo = saldo;
-        this.limite = 500.0;
-        this.limiteFixo = 500.0;
-        this.tipoConta = 0;
+    public String getTitular(){
+        return this.TITULAR;
     }
     
-    void saca(double valor) {
-        this.saldo = this.saldo - valor;
+    public int getNumero(){
+        return this.NUMERO;
     }
     
-    boolean sacaValor(double valor) {
+    public double getSaldo() {
+        return this.saldo;
+    }
+    
+    public double getLimite() {
+        return this.limite;
+    }
+    
+    public double getLimiteFixo() {
+        return this.limiteFixo;
+    }
+    
+    public int getTipoConta() {
+        return this.tipoConta;
+    }
+    
+    // Metodos
+    public boolean sacar(double valor) {
         if(this.saldo >= valor) {
             this.saldo = this.saldo - valor;  
             return true;
@@ -57,7 +75,7 @@ public class Conta {
         }
     }
     
-    void deposita(double valor) {
+    public void depositar(double valor) {
         if (this.limite == this.limiteFixo) {
             this.saldo += valor;
         } else {
@@ -67,27 +85,36 @@ public class Conta {
         }
     }
     
-    void exibeSaldo() {
-        System.out.println("Conta: " + this.numero);
-        System.out.println("Titular da Conta: " + this.titular);
-        System.out.println("Saldo disponível: " + this.saldo);
-        System.out.println("Limite disponível: " + this.limite);
-        System.out.println("Limite Máximo: " + this.limiteFixo);
-    }
-    
-    boolean transferencia(Conta destino, double valor) {
+    public boolean transferir(Conta destino, double valor) {
         if(this.saldo >= valor) {
             this.saldo = this.saldo - valor;  
-            destino.deposita(valor);
+            destino.depositar(valor);
             return true;
         } else if((this.saldo + this.limite) >= valor){
             double valorRestanteParaSacar = valor - this.saldo;
             this.saldo = 0.0;
             this.limite = this.limiteFixo - valorRestanteParaSacar;     
-            destino.deposita(valor);
+            destino.depositar(valor);
             return true;
         } else {
             return false;
         }
+    }
+    
+    public void exibeInformacoes() {
+        System.out.println("Conta: " + this.NUMERO);
+        System.out.println("Titular da Conta: " + this.TITULAR);
+        System.out.println("Saldo disponível: " + this.saldo);
+        System.out.println("Limite disponível: " + this.limite);
+        System.out.println("Limite Máximo: " + this.limiteFixo);
+    }
+    
+    public void alterarLimite(double valor) {
+        if (valor >= this.limiteFixo) {
+            this.limite += valor;
+        } else {
+            this.limite -= (this.limiteFixo - valor);
+        }
+        this.limiteFixo = valor;
     }
 }
--- aula_5/Programa_Conta_Private/src/programa_conta_private/Programa_Conta_Private.java
+++ aula_5/Programa_Conta_Private/src/programa_conta_private/Programa_Conta_Private.java
@@ -8,55 +8,17 @@ package programa_conta_private;
 public class Programa_Conta_Private {
 
     public static void main(String[] args) {
-        // Criando a conta 1
-        Conta minhaConta = new Conta();
-        minhaConta.titular = "Brasil Laranjal";
-        minhaConta.saldo = 1000.0;
-        minhaConta.limite = 2000.0;
-        minhaConta.limiteFixo = 2000.0;
-        minhaConta.numero = 123;
-        // Saque
-        double saque = 2000.0;
-        boolean consegui = minhaConta.sacaValor(saque);
-        System.out.println("-------------------------------------------");
-        System.out.println("Sacar: " + saque);
-        if (consegui == true) {
-            System.out.println("Saque realizado com sucesso!");
-            minhaConta.exibeSaldo();
-        } else {
-            System.out.println("Não foi possível realizar o saque");
-            minhaConta.exibeSaldo();
-        }
-        // Deposito
-        System.out.println("-------------------------------------------");
-        double deposito = 2000.0;
-        for(int qtde = 0; qtde < 48; qtde++) {
-            minhaConta.deposita(deposito);            
-        }
-        minhaConta.exibeSaldo();
-        System.out.println("-------------------------------------------");
-        // Criando a conta 1
-        Conta motoristaConta = new Conta();
-        motoristaConta.titular = "Sr. Motorista";
-        motoristaConta.saldo = 98000.0;
-        motoristaConta.limite = 2000.0;
-        motoristaConta.limiteFixo = 2000.0;
-        motoristaConta.numero = 171;
-        System.out.println("-------------------------------------------");
-        double transferir = 5000.0;
-        motoristaConta.transferencia(minhaConta, transferir);
-        motoristaConta.exibeSaldo();        
-        System.out.println("-------------------------------------------");
-        minhaConta.exibeSaldo();
         System.out.println("-------------------------------------------");
         System.out.println("-------------------------------------------");
         System.out.println("-------------------------------------------");
-        Conta conta_sem_nada = new Conta();
-        conta_sem_nada.exibeSaldo();
+        
         Conta conta_estudante = new Conta(111, "Cleber", 100.00);
-        conta_estudante.exibeSaldo();
+        conta_estudante.exibeInformacoes();
         Conta conta_especial = new Conta(222, "Suzana", 500000.00, 1000000.00, 2);
-        conta_especial.exibeSaldo();
-    }
-    
+        conta_especial.exibeInformacoes();
+        
+        System.out.println("-------------------------------------------");
+        System.out.println("-------------------------------------------");
+        System.out.println("-------------------------------------------");
+    }    
 }
