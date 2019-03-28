5285898318aa0de22f4e62a5e65e46046cdd0265
Diego Corrêa <diegocorrea.cc@gmail.com>
2019-03-27 23:18:04 -0300

    adicionando a atividade 2 de 2

---
+++ aula_6/BancoAplicacao/src/bancoaplicacao/BancoAplicacao.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bancoaplicacao;
+
+/**
+ *
+ * @author lowe
+ */
+public class BancoAplicacao {
+    private static final Banco DS_SA = new Banco("Banco Desenvolvimento de Sistemas S/A", 10);
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {        
+        BancoAplicacao.preencher_agencias();
+        BancoAplicacao.preencher_banco();
+        BancoAplicacao.DS_SA.imprimirAgencias();
+        BancoAplicacao.DS_SA.imprimirContas();
+        System.out.println("#################################################");
+        System.out.println("#################################################");
+        System.out.println("#################################################");
+        BancoAplicacao.deletar_agencia();
+        BancoAplicacao.DS_SA.imprimirContas();
+        BancoAplicacao.DS_SA.imprimirAgencias();
+    }
+    
+    public static void preencher_banco() {        
+        Conta nova_conta = new Conta(1111, "valeska", 500.0);
+        DS_SA.associarContaComAgencia(nova_conta, 999);
+        Conta nova_conta_2 = new Conta(222, "Suzana", 500000.00, 1000000.00, 2);
+        DS_SA.associarContaComAgencia(nova_conta_2, 666);
+        Conta nova_conta_3 = new Conta(33333, "Ana Banana", 100.00, 100.00);
+        DS_SA.associarContaComAgencia(nova_conta_3, 666);
+    }
+    
+    public static void preencher_agencias() {
+        Agencia nova_agencia_1 = new Agencia(666, 66);
+        DS_SA.associarAgenciaAoBanco(nova_agencia_1);
+        Agencia nova_agencia_2 = new Agencia(333, 10);
+        DS_SA.associarAgenciaAoBanco(nova_agencia_2);
+        Agencia nova_agencia_3 = new Agencia(999, 3);
+        DS_SA.associarAgenciaAoBanco(nova_agencia_3);
+    }
+    
+    public static void deletar_agencia() {
+        DS_SA.deletarAgencia(666);
+    }
+    
+}
---
+++ aula_6/BancoAplicacao/src/bancoaplicacao/Conta.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bancoaplicacao;
+
+/**
+ *
+ * @author lowe
+ */
+
+public class Conta {
+    private final int NUMERO;
+    private final String TITULAR;
+    private double saldo;
+    private double limite;
+    private double limiteFixo;
+    private int tipoConta; // [0 - 3] Estudante, Trabalhador, Especial, Ilimitado
+    
+    Conta(int numero, String titular, double saldo) {
+        this.NUMERO = numero;
+        this.TITULAR = titular;
+        this.saldo = saldo;
+        this.limite = 0.0;
+        this.limiteFixo = 0.0;
+        this.tipoConta = 1;
+    }
+
+    Conta(int numero, String titular, double saldo, double limiteFixo) {
+        this.NUMERO = numero;
+        this.TITULAR = titular;
+        this.saldo = saldo;
+        this.limite = limiteFixo;
+        this.limiteFixo = limiteFixo;
+        this.tipoConta = 1; // Trabalhador
+    }
+    
+    Conta(int numero, String titular, double saldo,double limiteFixo, int tipoConta) {
+        this.NUMERO = numero;
+        this.TITULAR = titular;
+        this.saldo = saldo;
+        this.limite = limiteFixo;
+        this.limiteFixo = limiteFixo;
+        this.tipoConta = tipoConta;
+    }
+    // gets
+    
+    public String getTitular(){
+        return this.TITULAR;
+    }
+    
+    public int getNumero(){
+        return this.NUMERO;
+    }
+    
+    public double getSaldo() {
+        return this.saldo;
+    }
+    
+    public double getLimite() {
+        return this.limite;
+    }
+    
+    public double getLimiteFixo() {
+        return this.limiteFixo;
+    }
+    
+    public int getTipoConta() {
+        return this.tipoConta;
+    }
+    
+    // Metodos
+    public boolean sacar(double valor) {
+        if(this.saldo >= valor) {
+            this.saldo = this.saldo - valor;  
+            return true;
+        } else if((this.saldo + this.limite) >= valor){
+            double valorRestanteParaSacar = valor - this.saldo;
+            this.saldo = 0.0;
+            this.limite = this.limiteFixo - valorRestanteParaSacar;            
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public void depositar(double valor) {
+        if (this.limite == this.limiteFixo) {
+            this.saldo += valor;
+        } else {
+            double paraDepositarNoLimite = this.limiteFixo - this.limite;
+            this.limite = this.limiteFixo;
+            this.saldo += (valor - paraDepositarNoLimite);
+        }
+    }
+    
+    public boolean transferir(Conta destino, double valor) {
+        if(this.sacar(valor)) {
+            destino.depositar(valor);
+            return true;
+        }
+        return false;
+    }
+    
+    public void exibeInformacoes() {
+        System.out.println("----------------------------------------------------");
+        System.out.println("Conta: " + this.NUMERO);
+        System.out.println("Titular da Conta: " + this.TITULAR);
+        System.out.println("Saldo disponível: " + this.saldo);
+        System.out.println("Limite disponível: " + this.limite);
+        System.out.println("Limite Máximo: " + this.limiteFixo);
+        System.out.println("Tipo da Conta: " + this.tipoConta);
+    }
+    
+    public void alterarLimite(double valor) {
+        if (valor >= this.limiteFixo) {
+            this.limite += valor;
+        } else {
+            this.limite -= (this.limiteFixo - valor);
+        }
+        this.limiteFixo = valor;
+    }
+}
